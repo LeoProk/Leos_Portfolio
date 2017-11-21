0eb609f84302e4e3b9c3091e0955e80dc19b96b0
Leonid Prokopenko <just_beta@hotmail.com>
2017-11-21 21:37:42 +0200

    Added webview to site permsions and icon to call

---
+++ app/src/main/java/leoprok/me/leosportfolio/factories/WebLoader.java
@@ -0,0 +1,30 @@
+package leoprok.me.leosportfolio.factories;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+import leoprok.me.leosportfolio.R;
+import leoprok.me.leosportfolio.interfaces.FactoryInterface;
+
+/**
+ * Load website to web view
+ */
+public class WebLoader implements FactoryInterface {
+
+    private WebView mWebView;
+    private Context mContext;
+
+    public WebLoader (WebView webView, Context context){
+        mWebView = webView;
+        mContext = context;
+    }
+
+    @Override
+    public Object doTask() {
+        //enabled javascript
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        //loads the url from string xml
+        mWebView.loadUrl(mContext.getString(R.string.web_site));
+        return null;
+    }
+}
