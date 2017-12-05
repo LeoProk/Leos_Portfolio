21a4856b9c39b1f95e53dcf3fc77dc0828ab5ea6
Leonid Prokopenko <just_beta@hotmail.com>
2017-12-05 20:07:26 +0200

    Finished Project

--- app/src/main/java/leoprok/me/leosportfolio/MainActivity.java
+++ app/src/main/java/leoprok/me/leosportfolio/MainActivity.java
@@ -17,6 +17,7 @@ import leoprok.me.leosportfolio.factories.FactoryBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,18 +46,11 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             //call when user click on the phone icon
             case R.id.call:
-                //check if the permission is enabled
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + R.string.phone));
-                    startActivity(intent);
-                }else {
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.CALL_PHONE},
-                            16);
-                }
-
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        1);
             default:
-            return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected(item);
         }
     }
 
@@ -65,13 +59,24 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case 16: {
+            case 1: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + R.string.phone));
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    startActivity(intent);
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+                }else{
 
                 }
                 return;
